9831f3c59cde2cdec6dcb27032251fe602803e02
aaronlin <aaron.lin@appier.com>
2016-08-13 11:28:04 +0800

    add header of api

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from bottle import route, run
+from bottle import Bottle, route, run, response
 import sqlite3
 import json
 
@@ -7,9 +7,23 @@ db = sqlite3.connect('../data/AgriDB.sqlite')
 cursor = db.cursor()
 resume_schema = ['pid', 'date', 'operation', 'detail', 'memo']
 
+app = Bottle()
+
+
+@app.hook('after_request')
+def enable_cors():
+    print("after_request hook")
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+
 @route('/resume/<pid>')
 def get_resume(pid):
-    cursor.execute('select * from OnP where PID = %s order by Date' % pid)
+    cursor.execute(
+        '''
+        select * from OnP where PID = %s order by Date
+        ''' % pid)
     result = []
     for resume in cursor.fetchall():
         result.append(dict(zip(resume_schema, resume)))
