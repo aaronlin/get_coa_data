c236fd988b92fc46a2412242fdd11a407a750023
aaronlin <aaron.lin@appier.com>
2016-08-13 11:14:56 +0800

    update api

--- api/main.py
+++ api/main.py
@@ -1,10 +1,20 @@
 from bottle import route, run
+import sqlite3
+import json
 
+
+db = sqlite3.connect('../data/AgriDB.sqlite')
+cursor = db.cursor()
+resume_schema = ['pid', 'date', 'operation', 'detail', 'memo']
 
 @route('/resume/<pid>')
 def get_resume(pid):
-    return ''
+    cursor.execute('select * from OnP where PID = %s order by Date' % pid)
+    result = []
+    for resume in cursor.fetchall():
+        result.append(dict(zip(resume_schema, resume)))
+    return json.dumps(result)
 
 
 if __name__ == '__main__':
-    run(host='localhost', port=8080)
+    run(host='0.0.0.0', port=8080)
